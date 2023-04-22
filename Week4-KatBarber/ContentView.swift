29049c6a450990068c1b37f6b7a2eb69fe5cdf0b
Kat Barber <kbarber876@gmail.com>
2023-04-22 19:26:38 -0400

    first commit

--- Week4-KatBarber/ContentView.swift
+++ Week4-KatBarber/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemPurple)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 20) {
+                
+                Text("Kat Barber")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                
+                Image("groupPicture")
+                    .resizable()
+                    .frame(width:260.0, height: 170.0)
+                
+                Text("This is a picture of me and my diving team at a competition! ")
+                    .padding()
+                
+
+                Button("reveal a fun fact") {
+                    let random = ["My favorite color is pink","I live in Pennsylvania", "I'm 18 years old", "All too well is my favorite Taylor song"]
+                    let randomNum = Int.random(in: 0..<random.count)
+                    print(random[randomNum])
+                    
+                    
+               
+                
+                }
+    }
+        
+        }
     }
 }
 
