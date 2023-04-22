bd21560e4780104c70e000ab3d1867c07cab9c35
Kat Barber <kbarber876@gmail.com>
2023-04-22 18:42:11 -0400

    Initial Commit

---
+++ Week4-KatBarber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Week4-KatBarber
+//
+//  Created by Barber, Kathryn Rose on 4/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week4-KatBarberTests/Week4_KatBarberTests.swift
@@ -0,0 +1,33 @@
+//
+//  Week4_KatBarberTests.swift
+//  Week4-KatBarberTests
+//
+//  Created by Barber, Kathryn Rose on 4/22/23.
+//
+
+import XCTest
+@testable import Week4_KatBarber
+
+class Week4_KatBarberTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
